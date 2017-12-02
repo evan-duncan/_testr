94c71dc751efd7fd376bd39977eb169e06d90147
Evan Duncan <evan.e.duncan@gmail.com>
2017-12-01 23:03:21 -0600

    Add notification system

--- client/src/actions/flashMessages.js
+++
@@ -1,4 +0,0 @@
-export const FLASH_SUCCESS = 'FLASH_SUCCESS';
-export const FLASH_DANGER = 'FLASH_DANGER';
-export const FLASH_INFO = 'FLASH_INFO';
-export const FLASH_WARNING = 'FLASH_WARNING';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,14 @@
 import user from './user';
 import app from './app';
+import notifications from './notifications';
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
     app,
+    notifications,
     user,
     router: routerReducer,
 });
 
 export default rootReducer;
-
