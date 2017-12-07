516b784879b251d4926ad5313134e56ca3184b20
Evan Duncan <evan.e.duncan@gmail.com>
2017-12-06 22:25:02 -0600

    Serialize/Deseerialize redux store to local storage
    It is necessary to have the ability to persist some of the state of the
    redux store for page refreshes. Without client-side persistence of
    select data there would be no simple way for a user to stay logged in if
    they were to refresh their page. This is just one example use-case but
    it is obvious that this is something that will be needed. There is also
    a throttling mechanism because the serialization to localstorage is
    expensive, so, at most, this can write to local storage once every
    second. This will allow us to capture most of the data, but still stay
    performant.

--- client/src/store.js
+++ client/src/store.js
@@ -4,14 +4,57 @@ import createHistory from 'history/createBrowserHistory';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import throttle from 'lodash.throttle';
 
 export const history = createHistory();
 const router = routerMiddleware(history);
 
-export default createStore(
+/**
+ * Attempt to pull state from localstorage and deserialize it back into redux store
+ */
+function loadState() {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (e) {
+        return undefined;
+    }
+}
+
+/**
+ * Serialize JSON and store it in localstorage
+ * @param {Object} state 
+ */
+function saveState(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch (e) {
+        /* Ignore write errors for now. Might be nice to log somewhere */
+    }
+}
+
+const persistedState = loadState();
+const store = createStore(
     rootReducer,
+    persistedState,
     composeWithDevTools(
         applyMiddleware(router, thunk)
     )
 );
 
+/**
+ * It is important to have the ability to persist some data on the client.
+ * In this case we are going to push state into localstorage at most 
+ * every second so that on a page refresh the state is back maintained.
+ * We don't persist the entire store. We selectively choose the properties
+ * within the store that we want to persist on the client.
+ */
+store.subscribe(throttle(() => saveState({
+    user: store.getState().user,
+}), 1000));
+
+export default store;
