4f8b31dcfe16f09c9993abab5045b7fbe40f11e5
Evan Duncan <evan.e.duncan@gmail.com>
2018-01-09 21:58:18 -0600

    Align GettingStarted component with the projects heading

--- client/src/Projects/GettingStarted.js
+++ client/src/Projects/GettingStarted.js
@@ -1,18 +1,21 @@
+import { Col } from 'react-flexbox-grid';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function GettingStarted({}) {
     return (
-        <div className="Projects--createNewWrapper">
-            <div className="Projects--headline">
-                <h2 className="Projects--hdg">Get Started with Testr</h2>
-                <p className="Projects--gettingStarted">
-                    If you're new to Testr check out our Getting Started guide to get up and running.
-                </p>
+        <Col xs={6} xsOffset={3}>
+            <div className="Projects--createNewWrapper">
+                <div className="Projects--headline">
+                    <h2 className="Projects--hdg">Get Started with Testr</h2>
+                    <p className="Projects--gettingStarted">
+                        If you're new to Testr check out our Getting Started guide to get up and running.
+                    </p>
+                </div>
+                <div className="Projects--createNewCta">
+                    <Link className="btn btn-primary" to={{ pathname: "/projects/new", from: window.location.pathname }}>Create New Project</Link>
+                </div>
             </div>
-            <div className="Projects--createNewCta">
-                <Link className="btn btn-primary" to={{ pathname: "/projects/new", from: window.location.pathname }}>Create New Project</Link>
-            </div>
-        </div>
+        </Col>
     );
 }
